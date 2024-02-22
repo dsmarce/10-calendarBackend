3d320e59740c1ade8753cc892b52e179a97d614c
marcela.dasilva <marcela.dasilva@assistcard.com>
2024-02-22 11:41:03 -0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //configuracion inicial de node
 const express = require('express');
 const {dbConnection} = require('./database/config');
+const cors= require('cors');
 require('dotenv').config();
 
 //console.log(process.env);
@@ -14,6 +15,9 @@ app.use(express.json());
 
 //base de datos
 dbConnection();
+
+//cords
+app.use(cors());
 //rutas
 //TODO: auth //crear, login, renew del token jwt
 app.use('/api/auth', require('./routes/auth'));
